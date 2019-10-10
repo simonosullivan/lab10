bc163ddbb47160fb56eee047883fa183f47f5971
Martin Kenirons <martin.kenirons@gmit.ie>
2019-10-10 14:29:24 +0100

    lab_upload

--- src/components/create.js
+++ src/components/create.js
@@ -1,10 +1,80 @@
 import React from 'react';
 
 class Create extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {Title:'',
+                  Year:'',
+                Poster:''};
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleMovieTitleChange = this.handleMovieTitleChange.bind(this);
+    this.handleMovieYearChange = this.handleMovieYearChange.bind(this);
+    this.handleMoviePosterChange = this.handleMoviePosterChange.bind(this);
+  }
+  
+  handleMovieTitleChange(e){
+    this.setState({Title: e.target.value});
+  }
+
+  handleMovieYearChange(e){
+    this.setState({Year: e.target.value});
+  }
+
+  handleMoviePosterChange(e){
+    this.setState({Poster: e.target.value});
+  }
+
+  handleSubmit(e){
+    alert(this.state.Title+ "      " + this.state.Year 
+    +"       "+ this.state.Poster);
+    e.preventDefault();
+    this.setState({Title:'',
+                  Year:'',
+                Poster:''});
+                
+  }
+
   render() {
     return (
       <div>
         <h1>Hello from Create component</h1>
+        <form onSubmit={this.handleSubmit}>
+        <div className='form-group'>
+          <label>Movie Title</label>
+          <input
+          type='text'
+          className='form-control'
+          value={this.state.Title}
+          onChange={this.handleMovieTitleChange}
+          ></input>
+        </div>
+        <div className='form-group'>
+          <label>Movie Year</label>
+          <input
+          type='text'
+          className='form-control'
+          value={this.state.Year}
+          onChange={this.handleMovieYearChange}
+          ></input>
+        </div>
+        <div className='form-group'>
+          <label>Movie Poster Url</label>
+          <textarea
+          row='3'
+          className='form-control'
+          value={this.state.Poster}
+          onChange={this.handleMoviePosterChange}
+          ></textarea>
+        </div>
+        <div>
+          <input
+          type="submit"
+          value="Add Movie">
+          </input>
+        </div>
+        </form>
       </div>
     );
   }
